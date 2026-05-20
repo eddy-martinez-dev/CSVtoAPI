b0b6aadbdbf0d08f163d879962ea848b291acb4d
eddy-martinez-dev <eddy.martinez.dev@gmail.com>
2026-05-20 19:43:07 -0400

    feat: configurar entity framework y contexto de base de datos

---
+++ API_REST/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_REST.Models;
+
+namespace API_REST.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
