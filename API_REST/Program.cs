49ff403bae90388c130b2eeef5f5d55216f68465
eddy-martinez-dev <eddy.martinez.dev@gmail.com>
2026-05-22 18:29:31 -0400

    fix: Correcion del bucle infinito por PostAsJsonAsync dentro del foreach

--- API_REST/Program.cs
+++ API_REST/Program.cs
@@ -11,15 +11,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
