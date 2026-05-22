547347aad58183d9666784938838320b46bebfbb
eddy-martinez-dev <eddy.martinez.dev@gmail.com>
2026-05-21 21:38:53 -0400

    feat: Agrego modelo de datos Persona para el cliente

---
+++ CSVtoAPI/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV_Cliente.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        required public string Name { get; set; }
+        required public string LastName { get; set; }
+        required public int Age { get; set; }
+        public DateTime Birthate { get; set; }
+    }
+}
