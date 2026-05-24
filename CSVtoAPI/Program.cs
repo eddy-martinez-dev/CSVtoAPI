dea5bfbdfa46de9c6379826fd3e744c909e69fdb
eddy-martinez-dev <eddy.martinez.dev@gmail.com>
2026-05-24 14:58:19 -0400

    fix: correcion de niveles de ruta relativa para lectura del CSV

--- CSVtoAPI/Program.cs
+++ CSVtoAPI/Program.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Json;
 
 
 string apiUrl = args.Length > 0 ? args[0] : "http://localhost:5164/api/Personas";
-string csvPath = Path.Combine("..","samples", "sample_personas_v2.csv");
+string csvPath = Path.Combine("..", "..", "..", "..", "samples", "sample_personas_v2.csv");
 
 Console.WriteLine(Path.GetFullPath(csvPath));
 using HttpClient client = new HttpClient();
