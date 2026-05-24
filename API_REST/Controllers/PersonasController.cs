4421ea705bdf83a2131561ec82c188169fe90532
eddy-martinez-dev <eddy.martinez.dev@gmail.com>
2026-05-23 22:15:24 -0400

    Eliminacion de metodos put y delete

--- API_REST/Controllers/PersonasController.cs
+++ API_REST/Controllers/PersonasController.cs
@@ -34,39 +34,7 @@ public class PersonasController : ControllerBase
         return persona;
     }
 
-    // PUT: api/Persona/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutPersona(int? id, Persona persona)
-    {
-        if (id != persona.Id)
-        {
-            return BadRequest();
-        }
-
-        _context.Entry(persona).State = EntityState.Modified;
-
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!PersonaExists(id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
-
-        return NoContent();
-    }
-
     // POST: api/Persona
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
     public async Task<ActionResult> PostPersonas([FromBody] List<Persona> personas)
     {
@@ -90,26 +58,5 @@ public class PersonasController : ControllerBase
         await _context.SaveChangesAsync();
 
         return Ok(new { message = "Records saved successfully" });
-    }
-
-    // DELETE: api/Persona/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeletePersona(int? id)
-    {
-        var persona = await _context.Personas.FindAsync(id);
-        if (persona == null)
-        {
-            return NotFound();
-        }
-
-        _context.Personas.Remove(persona);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-    }
-
-    private bool PersonaExists(int? id)
-    {
-        return _context.Personas.Any(e => e.Id == id);
     }
 }
